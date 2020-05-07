85a4e040b011bb6b389baa52a14ad7d9343c3da4
Manan Joshi <mananjoshi1995@gmail.com>
2020-05-07 18:10:50 -0400

    Updates

---
+++ precache-manifest.9d3572cf2a3019fb7d5302eb29c5051e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d786c38fa4ec2f5fb7a5e439f61c2fc4",
+    "url": "/yinzcam-assignment/index.html"
+  },
+  {
+    "revision": "974da34f13c7af9af30a",
+    "url": "/yinzcam-assignment/static/css/3.f4c64f63.chunk.css"
+  },
+  {
+    "revision": "8ed0af0088177612dddd",
+    "url": "/yinzcam-assignment/static/css/4.5b1b9b4f.chunk.css"
+  },
+  {
+    "revision": "6c4054116cc35f4de6ed",
+    "url": "/yinzcam-assignment/static/css/main.863f0874.chunk.css"
+  },
+  {
+    "revision": "230dc154ac8d0bf2c401",
+    "url": "/yinzcam-assignment/static/js/2.68dc7ea5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/yinzcam-assignment/static/js/2.68dc7ea5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "974da34f13c7af9af30a",
+    "url": "/yinzcam-assignment/static/js/3.6878de78.chunk.js"
+  },
+  {
+    "revision": "8ed0af0088177612dddd",
+    "url": "/yinzcam-assignment/static/js/4.d50a77b1.chunk.js"
+  },
+  {
+    "revision": "9bc9f8c6404de4656f3c",
+    "url": "/yinzcam-assignment/static/js/5.e30d78d6.chunk.js"
+  },
+  {
+    "revision": "6c4054116cc35f4de6ed",
+    "url": "/yinzcam-assignment/static/js/main.4f93b0a7.chunk.js"
+  },
+  {
+    "revision": "ccde3fe97da1784cdd02",
+    "url": "/yinzcam-assignment/static/js/runtime-main.e02d6171.js"
+  },
+  {
+    "revision": "3fa8ebef666a073fa210c8d9cfecfe73",
+    "url": "/yinzcam-assignment/static/media/back-button.3fa8ebef.svg"
+  },
+  {
+    "revision": "565408c28e1439aba6c3fa82018b8287",
+    "url": "/yinzcam-assignment/static/media/issue-opened.565408c2.svg"
+  },
+  {
+    "revision": "44b4d2b987dc459143fdcb4109b9cbc9",
+    "url": "/yinzcam-assignment/static/media/repo-forked.44b4d2b9.svg"
+  },
+  {
+    "revision": "c1d45b47a05d8ad3e424d47d7932d1da",
+    "url": "/yinzcam-assignment/static/media/star.c1d45b47.svg"
+  },
+  {
+    "revision": "4286a76d02a3c4c59ec214d3be3d4542",
+    "url": "/yinzcam-assignment/static/media/warning.4286a76d.svg"
+  },
+  {
+    "revision": "ab5c8e0127be30f62ab98a5e26f53334",
+    "url": "/yinzcam-assignment/static/media/watch.ab5c8e01.svg"
+  }
+]);
